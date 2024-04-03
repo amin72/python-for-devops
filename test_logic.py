619c95308eb1f5b01d67fe06a6c9ae1ab74cedc5
Amin <asaa47721@gmail.com>
2024-04-03 07:41:05 +0330

    test search_wiki function from logic module

--- test_logic.py
+++ test_logic.py
@@ -1,4 +1,9 @@
-from mylib.logic import wiki
+from mylib.logic import wiki, search_wiki
+
 
 def test_wiki():
-    assert 'god' in wiki()
+    assert "god" in wiki()
+
+
+def test_search_wiki():
+    assert 'Amin' in search_wiki('Amin')
