41c987515fec32c4f2647aadaef4e74672e717dd
Amin <asaa47721@gmail.com>
2024-04-03 07:41:24 +0330

    make cli for logic module

--- cli-fire.py
+++ cli-fire.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 
 import fire
-from mylib.logic import wiki
+from mylib import logic
 
 
-if __name__ == '__main__':
-    fire.Fire(wiki)
+if __name__ == "__main__":
+    fire.Fire(logic)
